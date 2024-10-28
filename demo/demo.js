27b1c2f71110fe3b3a6a9f05d66428ad8f8a1985
darcinmilena.becerra.contractor.cidentity <DARCINMILENA.BECERRA.CONTRACTOR.CIDENTITY@BBVA.COM>
2024-10-28 17:40:09 -0500

    Describe los cambios realizados

--- demo/demo.js
+++ demo/demo.js
@@ -2,5 +2,6 @@ import '@bbva-web-components/bbva-dev-demo-theme';
 import './css/demo.css.js';
 import '../pokemon-ux.js';
 import '@bbva-web-components/bbva-button-default/bbva-button-default.js';
+import { BbvaCoreModal } from '@bbva-web-components/bbva-core-modal';
 
 
--- src/PokemonUx.js
+++ src/PokemonUx.js
@@ -1,8 +1,9 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html } from 'lit-element';
 import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-helpers';
 import styles from './pokemon-ux.css.js';
 import '@bbva-web-components/bbva-button-default/bbva-button-default.js';
 import '@bbva-web-components/bbva-form-text/bbva-form-text.js';
+import "@pokedex/pokemon-dm/pokemon-dm.js";
 
 export class PokemonUx extends LitElement {
   static get properties() {
@@ -11,51 +12,28 @@ export class PokemonUx extends LitElement {
       searchTerm: { type: String },
       currentPage: { type: Number },
       totalPages: { type: Number },
-      pokemonsPerPage: { type: Number },
+      selectedPokemon: { type: Object },
+      showModal: { type: Boolean },
     };
   }
 
   constructor() {
     super();
     this.pokemons = [];
-    this.searchTerm = ''; // Inicializa el término de búsqueda vacío
+    this.searchTerm = '';
     this.currentPage = 1;
     this.pokemonsPerPage = 20;
     this.totalPages = 0;
+    this.selectedPokemon = null;
+    this.showModal = false;
   }
 
-  async fetchPokemons() {
-    try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=90');
-      const data = await response.json();
-
-      this.pokemons = await Promise.all(
-        data.results.map(async (pokemon) => {
-          const detailResponse = await fetch(pokemon.url);
-          const detailData = await detailResponse.json();
-          return {
-            name: detailData.name,
-            id: detailData.id,
-            image: detailData.sprites.other.dream_world.front_default,
-            type: detailData.types.map((type) => type.type.name).join(', '),
-          };
-        })
-      );
-
-      this.totalPages = Math.ceil(this.pokemons.length / this.pokemonsPerPage);
-    } catch (error) {
-      console.error('Error fetching Pokémon data:', error);
-    }
-  }
-
-  // Filtrar Pokémon según el término de búsqueda
   get filteredPokemons() {
     return this.pokemons.filter((pokemon) =>
       pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
   }
 
-  // Obtener los Pokémon de la página actual
   get paginatedPokemons() {
     const startIndex = (this.currentPage - 1) * this.pokemonsPerPage;
     return this.filteredPokemons.slice(startIndex, startIndex + this.pokemonsPerPage);
@@ -68,14 +46,65 @@ export class PokemonUx extends LitElement {
     }
   }
 
-  firstUpdated() {
-    this.fetchPokemons();
+  async firstUpdated() {
+    const pokemonDm = this.shadowRoot.querySelector('pokemon-dm');
+
+    pokemonDm.addEventListener('pokemon-data', (e) => {
+      this.pokemons = e.detail.pokemons;
+      this.totalPages = Math.ceil(this.pokemons.length / this.pokemonsPerPage);
+    });
+
+    await pokemonDm.fetchPokemons();
   }
 
-  // Actualizar el término de búsqueda
   updateSearchTerm(e) {
     this.searchTerm = e.target.value;
-    this.currentPage = 1; // Reiniciar a la primera página
+    this.currentPage = 1;
+  }
+
+  async handlePokemonClick(pokemon) {
+    this.selectedPokemon = pokemon;
+    this.selectedPokemon.evolutionChain = await this.fetchEvolutionChain(pokemon.id);
+    this.showModal = true;
+    document.body.classList.add('modal-open');
+  }
+
+  async fetchEvolutionChain(pokemonId) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const data = await response.json();
+    const speciesResponse = await fetch(data.species.url);
+    const speciesData = await speciesResponse.json();
+    const evolutionResponse = await fetch(speciesData.evolution_chain.url);
+    const evolutionData = await evolutionResponse.json();
+
+    return this.processEvolutionChain(evolutionData);
+  }
+
+  async processEvolutionChain(evolutionData) {
+    const evolutions = [];
+    let current = evolutionData.chain;
+
+    while (current) {
+      const name = current.species.name;
+      const pokemonDataResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const pokemonData = await pokemonDataResponse.json();
+
+      evolutions.push({
+        name: name,
+        image: pokemonData.sprites.other.dream_world.front_default,
+        type: pokemonData.types.map((type) => type.type.name).join(', ')
+      });
+
+      current = current.evolves_to[0];
+    }
+
+    return evolutions;
+  }
+
+  closeModal() {
+    this.showModal = false;
+    this.selectedPokemon = null;
+    document.body.classList.remove('modal-open');
   }
 
   static get styles() {
@@ -84,16 +113,15 @@ export class PokemonUx extends LitElement {
 
   render() {
     return html`
-  <h1 class="titulo-principal">Bienvenido a nuestra app de Pokémon</h1>
-<h2 class="titulo-secundario">Selecciona el Pokémon de tu preferencia</h2>
-
+      <h1 class="titulo-principal">Bienvenido a nuestra app de Pokémon</h1>
+      <h2 class="titulo-secundario">Selecciona el Pokémon de tu preferencia</h2>
 
       <bbva-form-text
         type="input"
         label="Ingresa el nombre"
         placeholder="Buscar..."
         optional-label=""
-        @input="${this.updateSearchTerm}"  <!-- Evento para capturar el texto -->
+        @input="${this.updateSearchTerm}"
       ></bbva-form-text>
 
       <div class="pokemon-container">
@@ -101,14 +129,14 @@ export class PokemonUx extends LitElement {
           (pokemon) => html`
             <div 
               class="pokemon-card" 
-              @click="${() => this.gotoData(pokemon)}"
               tabindex="0"
+              @click="${() => this.handlePokemonClick(pokemon)}"
             >
               <h1 class="titulo">${pokemon.name}</h1>
               <div class="cuerpo">
                 <img class="pokemon-image" src="${pokemon.image}" alt="${pokemon.name}" />
               </div>
-              <p class="tipo">${pokemon.type}</p>
+              <p class="tipo">${pokemon.type || 'Desconocido'}</p>
               <bbva-button-default>Ver más</bbva-button-default>
             </div>
           `
@@ -116,10 +144,38 @@ export class PokemonUx extends LitElement {
       </div>
 
       <div class="pagination">
-        <bbva-button-default @click="${() => this.changePage(-1)}" ?disabled="${this.currentPage === 1}">Previous</bbva-button-default>
+        <bbva-button-default @click="${() => this.changePage(-1)}" ?disabled="${this.currentPage === 1}">
+          Previous
+        </bbva-button-default>
         <span>Page ${this.currentPage} of ${this.totalPages}</span>
-        <bbva-button-default @click="${() => this.changePage(1)}" ?disabled="${this.currentPage === this.totalPages}">Next</bbva-button-default>
+        <bbva-button-default @click="${() => this.changePage(1)}" ?disabled="${this.currentPage === this.totalPages}">
+          Next
+        </bbva-button-default>
       </div>
+
+      ${this.showModal
+        ? html`
+            <div class="modal">
+              <h1 class="modal-title">${this.selectedPokemon.name}</h1>
+              <img class="modal-image" src="${this.selectedPokemon.image}" alt="${this.selectedPokemon.name}" />
+              <h2 class="modal-subtitle">Evoluciones:</h2>
+              <div class="evolution-container">
+                ${this.selectedPokemon.evolutionChain.map(evo => html`
+                  <div class="evolution-card">
+                    <h3 class="evolution-name">${evo.name}</h3>
+                    <img class="evolution-image" src="${evo.image}" alt="${evo.name}" />
+                    <p class="evolution-type">Tipo: ${evo.type}</p>
+                  </div>
+                `)}
+              </div>
+              <button class="modal-close-button" @click="${this.closeModal}">Cerrar</button>
+            </div>
+          `
+        : ''}
+      
+      <pokemon-dm></pokemon-dm>
     `;
   }
 }
+
+customElements.define('pokemon-ux', PokemonUx);
